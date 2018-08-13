d1327016193be6872d2c8dc212fffd5a339fe692
Brian Nelson and Kenneth du <kenneth@>
2018-08-13 14:05:37 -0700

    add sort list items by alphabetical order and uppercase first letter of list.

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,36 @@ $(document).ready(function() {
 
   $("#groceryList").submit(function(event) {
     event.preventDefault();
-    var gLists = ["item1", "item2", "item3", "item4", "item5"];
 
-    gLists.forEach(function(Glist) {
-      var listItem = $("input#" + Glist).val();
-      $(".output ul").append("<li>" + listItem + "</li>");
+    var gLists = ["item1", "item2", "item3", "item4", "item5", "item6"];
+    var inputList = [];
+    gLists.forEach(function(gList) {
+      var itemList = $("input#" + gList).val();
+
+      inputList.push(itemList);
+    })
+    // var item2 = $("input#item2").val();
+    // var item3 = $("input#item3").val();
+    // var item4 = $("input#item4").val();
+    // var item5 = $("input#item5").val();
+
+    var preSort = [];
+    // gLists = gLists.sort();
+
+    preSort = inputList.sort();
+
+
+    var upperCase = [];
+
+    preSort.forEach(function(word) {
+    var upperWord = word.charAt(0).toUpperCase() + word.substr(1).toLowerCase();
+    upperCase.push(upperWord);
+
+  })
+
+
+    upperCase.forEach(function(glist) {
+      $(".output ul").append("<li>" + glist + "</li>");
     });
 
   });
